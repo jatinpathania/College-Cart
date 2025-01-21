47473dfdaf107be1073af656688d62e32e05b39f
harash <harash.poriya@gmail.com>
2025-01-21 20:52:35 +0530

    Main file for server

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
-
-
+const signupRoute = require("./Routes/user");
+const connectMongdb = require("./db/db");
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 app.get("/",(res,req)=>{
     return req.send("Hello world");
 })
+connectMongdb();
+app.use(express.json());
+app.use("/api",signupRoute)
 
 app.listen(PORT,()=>{
     console.log(`Server Started at PORT: ${PORT}`);
 })
 
-// this code is successfully working
