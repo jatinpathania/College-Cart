82e4de37bdfcb8d0eb0f91bcfe0fdfb753c92c11
harash <harash.poriya@gmail.com>
2025-01-23 20:18:03 +0530

    Route product method delete product id by id

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -6,6 +6,7 @@ const productCreate = require("../Controllers/productAddForm")
 router.post("/product-create", productCreate.productAddForm,productCreate.createProduct);
 router.get("/all-product",productCreate.allProduct);
 router.get("/:id/product",productCreate.productById);
+router.delete("/:id/product",productCreate.productDeleteById);
 
 
 module.exports = router;
