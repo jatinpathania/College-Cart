623c398be45dddea0b0e11190fc471f4842840cc
harash <harash.poriya@gmail.com>
2025-01-23 20:19:49 +0530

    Add middlewares

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const connectMongdb = require("./db/db");
 require('dotenv').config();
 const PORT = process.env.PORT;
 const forgotPassword = require("./Routes/forgotpassword");
+const productRoute = require("./Routes/productAddRoute");
 
 app.get("/",(res,req)=>{
     return req.send("Hello world");
@@ -13,6 +14,7 @@ connectMongdb();
 app.use(express.json());
 app.use("/api",signupRoute);
 app.use("/api",forgotPassword);
+app.use("/api", productRoute);
 
 app.listen(PORT,()=>{
     console.log(`Server Started at PORT: ${PORT}`);
