80daae2c6f4261120dc860111e87db584b64ddde
harash <harash.poriya@gmail.com>
2025-01-23 20:17:23 +0530

    Route product method get product show id by id

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -5,6 +5,7 @@ const productCreate = require("../Controllers/productAddForm")
 
 router.post("/product-create", productCreate.productAddForm,productCreate.createProduct);
 router.get("/all-product",productCreate.allProduct);
+router.get("/:id/product",productCreate.productById);
 
 
 module.exports = router;
