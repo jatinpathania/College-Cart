29bfffb6a9eaa2b17ac6cb8a4ea9a28b57219a81
harash <harash.poriya@gmail.com>
2025-01-23 20:10:07 +0530

    Add product write the logic and use multer upload file

---
+++ Backend/Controllers/productAddForm.js
@@ -0,0 +1,60 @@
+const path = require("path")
+const fs = require("fs");
+const { v4: uuid4 } = require("uuid")
+const ProductAdd = require("../Model/productAddForm");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploadPath = path.join(process.cwd(), "/public/assets");
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = `${uuid4()}${path.extname(file.originalname)}`;
+        cb(null, uniqueName);
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (allowedTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid file type'), false);
+        }
+    }
+})
+
+exports.productAddForm = upload.single('image', { limits: { fileSize: 5 * 1024 * 1024 } });
+
+exports.createProduct = async (req, res) => {
+    try {
+
+        const { name, brand, category, hostleName, roomNumber, dayScholarContectNumber, prevAmount, newAmount, description } = req.body;
+        const image = req.file ? `/assets/${req.file.filename}` : null;
+
+        const createProduct = await ProductAdd.create({
+            name,
+            brand,
+            category,
+            hostleName,
+            roomNumber,
+            dayScholarContectNumber,
+            image,
+            description,
+            prevAmount,
+            newAmount
+        })
+
+        return res.status(201).json({ message: "Product created successful!", product: createProduct });
+
+    } catch (error) {
+        return res.status(400).json({ message: "Error during product created" });
+    }
+}
+
