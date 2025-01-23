5e3e78a7945fe5149665d6b9c477205854251c9a
harash <harash.poriya@gmail.com>
2025-01-23 20:16:51 +0530

    Route product method get all product show

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const productCreate = require("../Controllers/productAddForm")
 
 router.post("/product-create", productCreate.productAddForm,productCreate.createProduct);
+router.get("/all-product",productCreate.allProduct);
 
 
 module.exports = router;
