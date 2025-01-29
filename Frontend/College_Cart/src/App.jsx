5f8b8e6bddd4ca16d14fb60552e93889222a4241
harash <harash.poriya@gmail.com>
2025-01-29 20:17:03 +0530

    Add routes and route

--- Frontend/College_Cart/src/App.jsx
+++ Frontend/College_Cart/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+// import Header from "./Components/Header/Header"
+import './App.css'
+import { Provider } from 'react-redux'
+import Signup from './Components/Signup/Signup'
+import Store from './Components/SagaRedux/Store'
+import Signin from './Components/Signin/Signin'
+import {Routes,BrowserRouter,Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+     <Provider store={Store}>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
+      </Provider>
+    </div>
   )
 }
 
