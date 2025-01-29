2e4590c80e63dc031224daff1ee19b31ddd01d57
harash <harash.poriya@gmail.com>
2025-01-29 20:07:55 +0530

    Handle that the signup and login and verify-email post method

--- Frontend/College_Cart/src/Components/SagaRedux/Saga.jsx
+++ Frontend/College_Cart/src/Components/SagaRedux/Saga.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { put, call, takeLatest, all } from "redux-saga/effects"
+import {
+    signInUserFailed,
+    signInUserSuccess,
+    signUpUserFailed,
+    signUpUserSuccess,
+    emailVerifyFailed,
+    emailVerifySuccess,
+} from './Slice'
 
-const Saga = () => {
-  return (
-    <div>Saga</div>
-  )
+import * as api from "./api"
+
+function* signUpUserSaga(action) {
+    try {
+        const response = yield call(api.signUp, action.payload);
+        if (response.data.message) {
+            yield put(signUpUserSuccess({
+                message: response.data.message,
+                user: response.data.user || null,
+                token: response.data.token || null
+            }))
+        }
+    } catch (error) {
+        yield put(signUpUserFailed({
+            message: error.message,
+            error: error.message
+        }))
+    }
 }
 
+function* signInUserSaga(action) {
+    try {
+        const response = yield call(api.signIn, action.payload);
+        yield put(signInUserSuccess({
+            message: response.data.message,
+            user: response.data.user || null,
+            token: response.data.token || null
+        }))
+    } catch (error) {
+        yield put(signInUserFailed({
+            message: error.message,
+            error: error.message
+        }))
+    }
+}
+
+function* verifyEmailSaga(acton){
+    try {
+        const response = yield call(api.verifyEmail, acton.payload);
+        if (response.data.message) {
+            yield put(emailVerifySuccess({
+                message: response.data.message,
+                user: response.data.user || null,
+                token: response.data.token || null
+            }))
+        }
+    } catch (error) {
+        yield put(emailVerifyFailed({
+            message: error.message,
+            error: error.message
+        }))    }
+}
+
+function* Saga() {
+    yield takeLatest('app/signUpUser', signUpUserSaga);
+    yield takeLatest('app/signInUser', signInUserSaga);
+    yield takeLatest('app/emailVerify',verifyEmailSaga);
+}
 export default Saga
