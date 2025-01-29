bda16124c8515aa48838ec585462e2b6abc9a85d
harash <harash.poriya@gmail.com>
2025-01-29 20:09:35 +0530

    handle middlewares

--- Frontend/College_Cart/src/Components/SagaRedux/Store.jsx
+++ Frontend/College_Cart/src/Components/SagaRedux/Store.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { configureStore } from '@reduxjs/toolkit'
+import appReducer from "./Slice";
+import Saga from "./Saga";
+import createSagaMiddleware from "redux-saga"
 
-const Store = () => {
-  return (
-    <div>Store</div>
-  )
-}
+const sagaMiddleware = createSagaMiddleware();
+
+const Store = configureStore({
+    reducer:{
+        app:appReducer
+    },
+    middleware:(getDefaultMiddleware)=>  
+        getDefaultMiddleware().concat(sagaMiddleware),
+})
+
+sagaMiddleware.run(Saga);
 
 export default Store
