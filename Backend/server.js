25f12d125e3dcf24bde6f7ac81831cc0f92f2e28
harash <harash.poriya@gmail.com>
2025-01-29 20:13:49 +0530

    install cors

--- Backend/server.js
+++ Backend/server.js
@@ -6,11 +6,12 @@ require('dotenv').config();
 const PORT = process.env.PORT ?? 3000;
 const forgotPassword = require("./Routes/forgotpassword");
 const productRoute = require("./Routes/productAddRoute");
-
+const cors = require("cors")
 app.get("/",(res,req)=>{
     return req.send("Hello world");
 })
 connectMongdb();
+app.use(cors())
 app.use(express.json());
 app.use("/api",signupRoute);
 app.use("/api",forgotPassword);
