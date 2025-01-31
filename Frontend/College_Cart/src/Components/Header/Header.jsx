b9c3cd0426d4310fc802916711648d9ede6823e5
harash <harash.poriya@gmail.com>
2025-01-31 20:34:06 +0530

    Header create

--- Frontend/College_Cart/src/Components/Header/Header.jsx
+++ Frontend/College_Cart/src/Components/Header/Header.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IoMenu } from 'react-icons/io5';
+import { IoMdSearch } from 'react-icons/io';
+import './header.css';
+import Sidebar from '../Sidebar/Sidebar';
+import icon from "../../assets/logo.jpeg"
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
 
-export default Header
+  return (
+    <>
+      <div className="header">
+        <div className="header-left">
+          <button className="menu-button" onClick={() => setIsOpen(true)}>
+            <IoMenu size={40} />
+          </button>
+          <img 
+            src={icon} 
+            alt="Logo" 
+            className="logo"
+          />
+        </div>
+
+        <div className="search-container">
+          <IoMdSearch className="search-icon" />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="search-input"
+          />
+        </div>
+
+        <div className="header-right">
+          <div className="profile">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ41A81cAVOwJ6e58SZMxg_Fh-VSwnYIWb3Bw&s"
+              alt="Profile"
+              className="avatar"
+            />
+            <span className="username">Harash Poriya</span>
+          </div>
+        </div>
+      </div>
+      <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
+  );
+};
+
+export default Header;
