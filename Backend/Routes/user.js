fc28b4bc3883e4aa9f5021e9ebba80904c3d5cfc
harash <harash.poriya@gmail.com>
2025-02-01 11:39:08 +0530

    return user profile data

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -2,10 +2,18 @@ const express = require("express")
 const router = express.Router();
 
 const accountCreate = require("../Controllers/user")
+const {isAuthenticated} = require("../middleware/auth")
 
 router.post("/signup",accountCreate.signup);
 router.post("/verify-email", accountCreate.verifyEmail);
 router.post("/login",accountCreate.login);
-router.put('/password', accountCreate.updatePassword);
+router.put('/password', isAuthenticated, accountCreate.updatePassword);
+router.get("/user-profile", isAuthenticated, (req, res) => {
+    const { email, name, username } = req.user;
+    res.status(200).json({
+        success: true,
+        data: { email, name, username }
+    });
+});
 
 module.exports = router;
