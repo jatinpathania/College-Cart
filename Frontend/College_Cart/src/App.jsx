101874470aae71dab73600cdb0bd7bc34f27291b
harash <harash.poriya@gmail.com>
2025-02-01 12:00:03 +0530

    handle that the naivagte route and protected route

--- Frontend/College_Cart/src/App.jsx
+++ Frontend/College_Cart/src/App.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
-// import Header from "./Components/Header/Header"
 import './App.css'
 import { Provider } from 'react-redux'
+import { Routes, BrowserRouter, Route } from "react-router-dom"
 import Signup from './Components/Signup/Signup'
 import Store from './Components/SagaRedux/Store'
 import Signin from './Components/Signin/Signin'
-import {Routes,BrowserRouter,Route} from "react-router-dom"
 import ForgotPassword from './Components/ForgetPassword/Form'
 import Reset from './Components/ResetPassword/Reset'
 import Home from './Components/Home/Home'
+import PersistentAuth from './util/PersistentAuth'
+import ProtectedRoute from './util/ProtectedRoute'
+import Profile from './Components/Profile/Profile'
 
 const App = () => {
   return (
-    <div>
-     <Provider store={Store}>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Signin/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/forgotpassword' element={<ForgotPassword/>}/>
-        <Route path='/newPassword' element={<Reset/>}/>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-      </BrowserRouter>
-      </Provider>
-    </div>
+    <Provider store={Store}>
+      <PersistentAuth>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Signin/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+            <Route path='/newPassword' element={<Reset/>}/>
+            <Route path='/user-profile' element={<Profile/>}/>
+            <Route 
+              path='/' 
+              element={
+                <ProtectedRoute>
+                  <Home/>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </PersistentAuth>
+    </Provider>
   )
 }
 
 export default App
-
