5c003036cf89afc1f0eac5e45b6ffaae48c0a56a
harash <harash.poriya@gmail.com>
2025-02-01 11:41:54 +0530

    Add cookie parser and cors cretentials

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,21 @@ require('dotenv').config();
 const PORT = process.env.PORT ?? 3000;
 const forgotPassword = require("./Routes/forgotpassword");
 const productRoute = require("./Routes/productAddRoute");
+const cookieParser = require('cookie-parser');
 const cors = require("cors")
+
+
 app.get("/",(res,req)=>{
     return req.send("Hello world");
 })
 connectMongdb();
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true 
+}));
+
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api",signupRoute);
 app.use("/api",forgotPassword);
 app.use("/api", productRoute);
