c93301a6e2af52e67f9f7ca630fb2c1f48e3fb05
harash <harash.poriya@gmail.com>
2025-02-01 11:40:22 +0530

    Expires 1 day

--- Backend/Token/jwt.js
+++ Backend/Token/jwt.js
@@ -2,5 +2,5 @@ const jwt = require("jsonwebtoken")
 require('dotenv').config();
 
 exports.jwtToken = (userId)=>{
-   return jwt.sign({userId:userId},process.env.JWT_SECRET,{expiresIn:"2 day"});
+   return jwt.sign({userId:userId},process.env.JWT_SECRET,{expiresIn:"1d"});
 }
