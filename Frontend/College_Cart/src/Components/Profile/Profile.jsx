1764d0188640a44df5e23b4251be59ed4238270e
harash <harash.poriya@gmail.com>
2025-02-02 14:01:25 +0530

    This component is user profile

--- Frontend/College_Cart/src/Components/Profile/Profile.jsx
+++ Frontend/College_Cart/src/Components/Profile/Profile.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import Header from "../Header/Header"
-const Profile = () => {
-  return (
-    <div>
-        <Header/>
-    </div>
-  )
-}
+import React, { useContext } from 'react';
+import Header from '../Header/Header';
+import { UserDataContext } from '../Header/context';
+import "./profile.css"
 
-export default Profile
+
+const Profile = () => {
+    const { data } = useContext(UserDataContext);
+    
+    return (
+        <>
+            <Header />
+            <div className="profile-page">
+                <div className="profile-card">
+                    <div className="profile-header">
+                        <div className="profile-image-container">
+                            <img 
+                                className="profile-image" 
+                                src={data.profileImage || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+                                alt={data.name} 
+                            />
+                        </div>
+                        <div className="profile-info">
+                            <h1 className="profile-name">{data.name}</h1>
+                            <p className="profile-username">@{data.username}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Profile;
