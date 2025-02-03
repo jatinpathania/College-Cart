2bf518dd95f55bd1034e1f9d13417c33c0595b7f
harash <harash.poriya@gmail.com>
2025-02-03 11:53:01 +0530

    add method patch for profile image upload

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router();
+const upload = require("../Config/multer")
+
 
 const accountCreate = require("../Controllers/user")
 const {isAuthenticated} = require("../middleware/auth")
@@ -9,11 +11,13 @@ router.post("/verify-email", accountCreate.verifyEmail);
 router.post("/login",accountCreate.login);
 router.put('/password', isAuthenticated, accountCreate.updatePassword);
 router.get("/user-profile", isAuthenticated, (req, res) => {
-    const { email, name, username,_id,profileImage } = req.user;
+    const { email, name, username,_id,profileImage, createdAt, updatedAt } = req.user;
     res.status(200).json({
         success: true,
-        data: { email, name, username,_id,profileImage }
+        data: { email, name, username,_id,profileImage, createdAt, updatedAt }
     });
 });
+
+router.patch("/update-profile/:id", isAuthenticated, upload.single('profileImage'), accountCreate.updateProfileImageAndUserName);
 
 module.exports = router;
