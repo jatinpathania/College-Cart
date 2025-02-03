d1f309bed1ca00842d2db1107589f9eec3dc1446
harash <harash.poriya@gmail.com>
2025-02-03 11:52:18 +0530

    update function name and add authenticated

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router();
 
-const productCreate = require("../Controllers/productAddForm")
+const productCreate = require("../Controllers/productAddForm");
+const { isAuthenticated } = require("../middleware/auth");
 
-router.post("/product-create", productCreate.productAddForm,productCreate.createProduct);
-router.get("/all-product",productCreate.allProduct);
-router.get("/:id/product",productCreate.productById);
-router.delete("/:id/product",productCreate.productDeleteById);
-router.put("/:id/product-update",productCreate.productUpdateById);
+router.post("/product-create", isAuthenticated, productCreate.productAddForm,productCreate.createProduct);
+router.get("/all-product",productCreate.getAllProduct);
+router.get("/:id/product",productCreate.getProductById);
+router.delete("/:id/product", isAuthenticated, productCreate.productDeleteById);
+router.put("/:id/product-update",isAuthenticated, productCreate.updateProduct);
 
 module.exports = router;
