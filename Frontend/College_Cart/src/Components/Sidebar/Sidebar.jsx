63302af5de4b6898f4dca93205eea37e7bd2aa84
harash <harash.poriya@gmail.com>
2025-02-03 11:44:43 +0530

    Add framer motion

--- Frontend/College_Cart/src/Components/Sidebar/Sidebar.jsx
+++ Frontend/College_Cart/src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState,useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { IoMdClose } from 'react-icons/io';
 import './sidebar.css';
 import icon from "../../assets/logo.jpeg";
@@ -7,109 +7,183 @@ import { toast } from 'react-toastify';
 import { logout } from '../SagaRedux/Slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserDataContext } from '../Header/context';
+import { motion } from "framer-motion"
+import { SquarePlus } from 'lucide-react';
 
 const Sidebar = ({ isOpen, onClose }) => {
   const navigate = useNavigate()
-  const {status, token, user} = useSelector((state)=>state.app);
-  const {data} = useContext(UserDataContext)
+  const { status, token, user } = useSelector((state) => state.app);
+  const { data } = useContext(UserDataContext)
   const dispatch = useDispatch();
-  const [showLogout, setShowLogout] = useState(false); 
+  const [showLogout, setShowLogout] = useState(false);
   const email = data.email || "";
   const [localPart, domainPart] = email.includes("@") ? email.split("@") : ["", ""];
+  const [showAddProducts, setShowAddProducts] = useState(false)
+  const [showMessages, setshowMessages] = useState(false);
 
 
-   const handleLogout=()=>{
-     dispatch(logout());
-     toast.success("Logout success")
-     setTimeout(() => {
+  const handleLogout = () => {
+    dispatch(logout());
+    toast.success("Logout success")
       navigate("/login")
-     }, 5000);
-   }
-
-   useEffect(() => {
+  }
+  
+  useEffect(() => {
     if (token && user && status === 'success') {
       setShowLogout(true);
+      setShowAddProducts(true)
+      setshowMessages(true);
     } else {
       setShowLogout(false);
+      setShowAddProducts(false)
+      setshowMessages(false);
     }
   }, [token, user, status]);
+
+
+  const sidebarVaraints = {
+    open: { x: 0, opacity: 1, scale: 1 },
+    closed: { x: "-100%", opacity: 0, scale: 0.9 }
+  }
+
+  const backdropVaraints = {
+    open: { opacity: 0.6 },
+    closed: { opacity: 0 },
+  }
+
+  const containerVaraints = {
+    open: {
+      transition: {
+        staggerChildren: 0.2,
+        dealyChildren: 0.1,
+      }
+    },
+    closed: {
+      transition: {
+        staggerChildren: 0.2,
+        dealyChildren: 0,
+      }
+    }
+  }
+
+  const itemsVaraints = {
+    open: { opacity: 1, y: 0, scale: 1 },
+    closed: { opacity: 0, y: -20, scale: 0.95 }
+  }
 
 
   return (
     <>
       {isOpen && <div className="sidebar-overlay" onClick={onClose} />}
-      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <motion.div className={`sidebar ${isOpen ? 'open' : ''}`}
+        initial="closed"
+        animate={isOpen ? "open" : "closed"}
+        variants={sidebarVaraints}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+      >
+
         <div className="sidebar-header">
           <img
             src={icon}
             alt="Logo"
             className="sidebar-logo"
           />
-          <button className="close-button" onClick={onClose}>
+          <motion.button className="close-button" onClick={onClose}
+            initial="closed"
+            animate={isOpen ? "open" : "closed"}
+            variants={backdropVaraints}
+            transition={{ duration: 0.3 }}
+            whileHover={{ scale: 1.1 }}
+          >
             <IoMdClose size={24} />
-          </button>
+          </motion.button>
         </div>
 
-        <div className="sidebar-content">
-          <div className="menu-item" onClick={()=>navigate("/dashboard")}>
+        <motion.div className="sidebar-content"
+          variants={containerVaraints}
+          initial="closed"
+          animate={isOpen ? "open" : "closed"}
+        >
+          <motion.div className="menu-item" onClick={() => navigate("/dashboard")}
+            variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 300, damping: 15 }}
+          >
             <span className="menu-icon">🏠</span>
             <span className="menu-title">Dashboard</span>
-          </div>
+          </motion.div>
 
-          <div className="menu-item" onClick={()=>navigate(`/${data._id}/user-profile`)}>
+          <motion.div variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 300, damping: 15 }}
+            className="menu-item" onClick={() => navigate(`/${data._id}/user-profile`)}>
             <span className="menu-icon">👤</span>
             <span className="menu-title">Profile</span>
-          </div>
+          </motion.div>
 
-          <div className="menu-item">
+          <motion.div variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 300, damping: 15 }}
+            className="menu-item">
             <span className="menu-icon">⚙️</span>
             <span className="menu-title">Settings</span>
-          </div>
+          </motion.div>
 
-          <div className="menu-item">
-            <span className="menu-icon">✉️</span>
-            <span className="menu-title">Messages</span>
-          </div>
+         { showMessages && 
+          <motion.div className="menu-item" onClick={()=>navigate("/messages")}
+          variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+          transition={{ type: "spring", stiffness: 300, damping: 15 }} >
+          <span className="menu-icon">✉️</span>
+          <span className="menu-title">Messages</span>
+        </motion.div>}
 
-          <div className="menu-item">
+          <motion.div className="menu-item"
+            variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 300, damping: 15 }} onClick={()=>navigate("/all-products")}>
             <span className="menu-icon">📊</span>
-            <span className="menu-title">Product</span>
-          </div>
-
-          <div className="menu-item">
-            <span className="menu-icon">❓</span>
-            <span className="menu-title">Help</span>
-          </div>
+            <span className="menu-title">Products</span>
+          </motion.div>
 
          {
-          showLogout ? 
-         (
-          <div className="menu-item" onClick={handleLogout}>
-          <span className="menu-icon">🚪</span>
-          <span className="menu-title">Logout</span>
-        </div>
-         ) :(
-          <div className="menu-item" onClick={()=>navigate("/login")}>
-          <span className="menu-icon">🚪</span>
-          <span className="menu-title">Signup / Signin</span>
-        </div>
-         )
+          showAddProducts && 
+          <motion.div className="menu-item" onClick={()=>navigate(`/${data._id}/add-products-user`)}
+          variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+          transition={{ type: "spring", stiffness: 300, damping: 15 }}>
+          <span className="menu-icon"><SquarePlus color='red' /></span>
+          <span className="menu-title">Add Products</span>
+        </motion.div>
          }
-        </div>
-       
+
+          {
+            showLogout ?
+              (
+                <motion.div className="menu-item" onClick={handleLogout}
+                  variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+                  transition={{ type: "spring", stiffness: 300, damping: 15 }}>
+                  <span className="menu-icon">🚪</span>
+                  <span className="menu-title">Logout</span>
+                </motion.div>
+              ) : (
+                <motion.div className="menu-item" onClick={() => navigate("/login")}
+                  variants={itemsVaraints} whileHover={{ scale: 1.05 }}
+                  transition={{ type: "spring", stiffness: 300, damping: 15 }}>
+                  <span className="menu-icon">🚪</span>
+                  <span className="menu-title">Signup / Signin</span>
+                </motion.div>
+              )
+          }
+        </motion.div>
+
         <div className="sidebar-footer">
-  <div className="profileContainer">
-    <div>
-      <img className="image" src={data.profileImage || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'} alt="Profile" />
-    </div>
-    <div>
-      <span className="name">{data.name || 'Jhon Doe'}</span><br />
-      <span className="email">{localPart || 'jhondow1215.be23'} <br />@{domainPart || 'chitkarauniversity.edu.in'}</span>
-    </div>
-  </div>
-</div>
-      </div>
-      
+          <div className="profileContainer">
+            <div>
+              <img className="image" src={data.profileImage || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'} alt="Profile" />
+            </div>
+            <div>
+              <span className="name">{data.name || 'Jhon Doe'}</span><br />
+              <span className="email">{localPart || 'jhondow1215.be23'} <br />@{domainPart || 'chitkarauniversity.edu.in'}</span>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+
     </>
   );
 };
