d18aeb35d84abc229c6c57c9c78d15e8fd948c95
harash <harash.poriya@gmail.com>
2025-02-05 23:38:40 +0530

    route change

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -7,7 +7,7 @@ const { isAuthenticated } = require("../middleware/auth");
 router.post("/product-create", isAuthenticated, productCreate.productAddForm,productCreate.createProduct);
 router.get("/all-product",productCreate.getAllProduct);
 router.get("/:id/product",productCreate.getProductById);
-router.delete("/:id/product", isAuthenticated, productCreate.productDeleteById);
+router.delete("/:id/product-delete", isAuthenticated, productCreate.productDeleteById);
 router.put("/:id/product-update",isAuthenticated, productCreate.updateProduct);
 
 module.exports = router;
