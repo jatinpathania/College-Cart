a91da5948843780c459af7013acab9024c2e1863
harash <harash.poriya@gmail.com>
2025-02-06 12:58:51 +0530

    add toast hot-toast

--- Frontend/College_Cart/src/Components/Signup/MessageHandler.jsx
+++ Frontend/College_Cart/src/Components/Signup/MessageHandler.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import toast, { Toaster } from 'react-hot-toast';
 import { resetState } from '../SagaRedux/Slice';
 
 const MessageHandler = () => {
@@ -11,20 +10,20 @@ const MessageHandler = () => {
     let timer;
     if(status === 'success' && message){
       toast.success(message,{
-        position: "top-right",
+        position: "top-center",
         timer : setTimeout(() => dispatch(resetState()), 4000),
       })
     }
 
     if(status === 'failed' && error){
       toast.error(error,{
-        position: "top-right",
+        position: "top-center",
         timer :setTimeout(() => dispatch(resetState()), 4000)
       })
     }
     return () => clearTimeout(timer); 
   },[status,error,message,dispatch]);
-  return <ToastContainer/>
+  return <Toaster/>
 }
 
 export default MessageHandler
