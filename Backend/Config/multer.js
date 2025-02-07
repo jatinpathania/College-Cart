62394264b68d6806b56b849d422e57ba121bfb08
harash <harash.poriya@gmail.com>
2025-02-07 15:04:34 +0530

    handle multer

--- Backend/Config/multer.js
+++ Backend/Config/multer.js
@@ -1,30 +1,19 @@
 const multer = require('multer');
 const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/'); 
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-        cb(null, true);
-    } else {
-        cb(new Error('Not an image! Please upload an image.'), false);
-    }
-};
+const storage = multer.memoryStorage()
 
 const upload = multer({
     storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: 5 * 1024 * 1024 
-    }
-});
+    fileFilter: (req, file, cb) => {
+        console.log("Received file in multer:", file);
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (allowedTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid file type'), false);
+        }
+    },
+     limits: { fileSize: 5 * 1024 * 1024 } 
+})
 
 module.exports = upload;
