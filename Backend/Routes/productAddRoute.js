f61a59dc6cf4868819b24de88b57e16a73fc0e37
harash <harash.poriya@gmail.com>
2025-02-09 15:31:30 +0530

    Add route for public data

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -6,6 +6,7 @@ const { isAuthenticated } = require("../middleware/auth");
 
 router.post("/product-create", isAuthenticated, productCreate.productAddForm,productCreate.createProduct);
 router.get("/all-product", isAuthenticated, productCreate.getAllProduct);
+router.get("/public-products", productCreate.getPublicProducts);
 router.get("/:id/product",productCreate.getProductById);
 router.delete("/:id/product-delete", isAuthenticated, productCreate.productDeleteById);
 router.put("/:id/product-update",isAuthenticated, productCreate.updateProduct);
