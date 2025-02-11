7118a8b02dfbad303336ef2338fb5ad88d1319eb
harash <harash.poriya@gmail.com>
2025-02-11 09:54:53 +0530

    route set

--- Backend/Routes/productAddRoute.js
+++ Backend/Routes/productAddRoute.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const productCreate = require("../Controllers/productAddForm");
 const { isAuthenticated } = require("../middleware/auth");
+const upload = require("../Config/multer")
 
 router.post("/product-create", isAuthenticated, productCreate.productAddForm,productCreate.createProduct);
 router.get("/all-product", isAuthenticated, productCreate.getAllProduct);
 router.get("/public-products", productCreate.getPublicProducts);
 router.get("/:id/product",productCreate.getProductById);
 router.delete("/:id/product-delete", isAuthenticated, productCreate.productDeleteById);
-router.put("/:id/product-update",isAuthenticated, productCreate.updateProduct);
+router.put("/:id/product-update",isAuthenticated, upload.single('image'), productCreate.updateProduct);
 
 module.exports = router;
