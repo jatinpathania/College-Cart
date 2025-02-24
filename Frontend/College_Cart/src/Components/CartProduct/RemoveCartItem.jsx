17318694001a4d0883b37b47518a61ab68d0df06
harash <harash.poriya@gmail.com>
2025-02-24 10:00:23 +0530

    this file is dialog show and handle the remove item

---
+++ Frontend/College_Cart/src/Components/CartProduct/RemoveCartItem.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import React, { useRef,useEffect } from 'react'
+const backend_url = import.meta.env.VITE_BACKEND_API_URL;
+import toast, { Toaster } from 'react-hot-toast';
+import styles from "./remove.module.css"
+import { motion } from 'framer-motion';
+
+const RemoveCartItem = ({ isOpen, onClose, cartItemId }) => {
+//    console.log(cartItemId)
+   const dialogRef = useRef(null);
+     useEffect(() => {
+       const dialogElement = dialogRef.current;
+       if (isOpen) {
+         dialogElement.showModal();
+       } else {
+         dialogElement.close();
+       }
+     }, [isOpen]);
+   
+     const handleClose = (e) => {
+       e.preventDefault();
+       onClose();
+     };
+     
+    const handleDeleteProduct = async () => {
+        try {
+            await axios.delete(`${backend_url}/${cartItemId}/cart-product-delete`)
+            toast.success("Cart product delete Successfull")
+            onClose()
+        } catch (error) {
+            console.log("Error", error);
+            toast.error("cart product delete during error");
+        }
+    }
+    return (
+        <>
+           <dialog ref={dialogRef} 
+                 className={styles.dialogContainer}
+                 onCancel={handleClose}
+               >
+           
+                {isOpen && (
+                    <motion.div className={styles.openDialog}
+                        initial={{ opacity: 0, scale: 0.9 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.9 }}
+                        transition={{ duration: 0.3 }}>
+                        <h2 className={styles.deleteText}>Delete Product</h2>
+                        <p className={styles.deleteConfirmation}>
+                            Are you sure you want to delete this product?
+                        </p>
+                        <div className={styles.buttonContainer}>
+                            <button
+                                className={styles.cancelButton}
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                className={styles.deleteButton}
+                                onClick={handleDeleteProduct}
+                            >
+                                Remove
+                            </button>
+                        </div>
+                    </motion.div>
+                )}
+            {/* </Animation> */}
+            </dialog>
+            <Toaster />
+        </>
+    )
+}
+
+export default RemoveCartItem
