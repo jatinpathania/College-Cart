5cb21139f514652cafb41aa4d44baa5aa9134109
harash <harash.poriya@gmail.com>
2025-02-24 09:54:40 +0530

    route add for cart item remove and method delete

--- Backend/Routes/cartProductAdd.js
+++ Backend/Routes/cartProductAdd.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { cartProduct, getAllCartProduct } = require("../Controllers/cartProductAdd");
+const { cartProduct, getAllCartProduct, deleteProductByIdForCart } = require("../Controllers/cartProductAdd");
 const { isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/cartProductAdd", isAuthenticated, cartProduct);
 router.get("/all-cart-product", isAuthenticated,getAllCartProduct);
+router.delete("/:id/cart-product-delete", deleteProductByIdForCart);
 
 module.exports = router;
