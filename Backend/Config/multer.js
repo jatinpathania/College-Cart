c5f59345094b0a067820d81123804eda432b13dc
harash <harash.poriya@gmail.com>
2025-02-26 13:44:32 +0530

    remove image type

--- Backend/Config/multer.js
+++ Backend/Config/multer.js
@@ -6,12 +6,12 @@ const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
      //   console.log("Received file in multer:", file);
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-        if (allowedTypes.includes(file.mimetype)) {
+        // const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        // if (allowedTypes.includes(file.mimetype)) {
             cb(null, true);
-        } else {
-            cb(new Error('Invalid file type'), false);
-        }
+        // } else {
+        //     cb(new Error('Invalid file type'), false);
+        // }
     },
      limits: { fileSize: 5 * 1024 * 1024 } 
 })
