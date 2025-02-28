8c66c55aabde1021d7b0df5f312ed1b7dc13c076
harash <harash.poriya@gmail.com>
2025-02-28 23:59:56 +0530

    add context totalQuantity

--- Frontend/College_Cart/src/Components/Header/context.jsx
+++ Frontend/College_Cart/src/Components/Header/context.jsx
@@ -5,9 +5,10 @@ const UserDataContext = createContext();
 const UserDataProvider =({children})=>{
   const [data, setData] = useState('');
   const [searchQuery, setSearchQuery] = useState("");
+  const [totalQuantity, setTotalQuantity] = useState(0)
 
   return(
-    <UserDataContext.Provider value={{data,setData,searchQuery,setSearchQuery}}>
+    <UserDataContext.Provider value={{data,setData,searchQuery,setSearchQuery, totalQuantity, setTotalQuantity}}>
         {children}
     </UserDataContext.Provider>
   )
