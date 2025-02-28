53a895d08df92460ecaf1a68fc0ca977722ba609
harash <harash.poriya@gmail.com>
2025-03-01 00:05:04 +0530

    add comment

--- Frontend/College_Cart/src/main.jsx
+++ Frontend/College_Cart/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { UserDataProvider } from './Components/Header/context.jsx'
+// import { CartProductProvider } from './Components/Product/ProductContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <UserDataProvider>
+    // <CartProductProvider>
+      <UserDataProvider>
     <App />
     </UserDataProvider>
+    // {/* </CartProductProvider> */}
   // </StrictMode>,
 )
