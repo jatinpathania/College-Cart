def02e08c949a9bda71e97a9acd3216df7e36fd4
harash <harash.poriya@gmail.com>
2025-03-02 09:28:41 +0530

    handle api and add token for authorization

--- Frontend/College_Cart/src/Components/SagaRedux/api.js
+++ Frontend/College_Cart/src/Components/SagaRedux/api.js
@@ -45,7 +45,11 @@ export const passwordNewSet = (newPassword)=>API.put("/password",newPassword);
 export const getUserProfile = (getUserProfile)=>API.get("/user-profile",getUserProfile);
 export const productCreate = (newProduct) =>API.post("/product-create",newProduct)
 export const getAllProduct = (allProduct)=>API.get("/all-product",allProduct)
-export const getProductDetails = (productId) => API.get(`/${productId}/product`);
+export const getProductDetails = (productId) =>{
+  const token = getToken();
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  return API.get(`/${productId}/product`,{headers});
+}
 export const cartProductAdd = (cartProduct) => API.post("/cartProductAdd", cartProduct)
 export const productCartAll = (cartAllProduct) => API.get("/all-cart-product", cartAllProduct)
 
