68b15e2c644c66910b99e791333e920e1b06231a
Jatin <jatinpathania1@gmail.com>
2025-03-07 23:33:00 +0530

    added css to signin page for pc and mobile device

--- Frontend/College_Cart/src/Components/Signin/Signin.jsx
+++ Frontend/College_Cart/src/Components/Signin/Signin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState,  } from 'react';
-import './signin.css';
+import styles from './signin.module.css';
 import { useDispatch, useSelector,  } from 'react-redux';
 import {  signInUser} from '../SagaRedux/Slice';
 import MessageHandler from '../Signup/MessageHandler';
@@ -40,49 +40,63 @@ const Signin = () => {
 
 
   return (
-    <div className="formContainer">
-      <form className="form" onSubmit={handleSubmit}>
-        <p className="signupText">Signin</p>
-        <p className="textInformation">Enter your information to signin an account</p>
+    <div className={styles.bodyContainer}>
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>
+      <div className={styles.formContainer}>
+      <div className={styles.box}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <p className={styles.signupText}>Signin</p>
+        <p className={styles.textInformation}>Enter your information to signin an account</p>
 
 
-        <div className="emailContainer">
-          <label htmlFor="email" className="email" style={{color:"white"}}>Email</label><br />
+        <div className={styles.emailContainer}>
+          <div className={styles.inputBox}>
           <input
             id="email"
             name="email"
-            placeholder="@example.com"
             type="email"
-          //  required
+            required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <span>Email</span>
+          <i></i>
+          </div>
         </div>
 
-        <div className="passwordContainer">
-          <label htmlFor="password" className="password">Password</label><br />
+        <div className={styles.passwordContainer}>
+          <div className={styles.inputBox}>
           <input
             id="password"
             name="password"
-            placeholder="password"
             type="password"
-          //  required
+            required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <p className='forgotpassword' onClick={()=>navigate('/forgotpassword')}>Forgot password?</p>
+          <span>Password</span>
+          <i></i>
+          </div>
+          <p className={styles.forgotpassword} onClick={()=>navigate('/forgotpassword')}>Forgot password?</p>
         </div>
 
-        <div className="btnContainer">
+        <div className={styles.btnContainer}>
           <button type="submit"> {isLoading ? 'Signing In...' : 'Sign In'}</button>
         </div>
-        <div className="alreadyAccountContainer">
-          <p className="alreadyAccount">
-            Don't have an account?<span className="alreadySignin" onClick={()=>navigate('/signup')}> Sign up</span>
+        <div className={styles.alreadyAccountContainer}>
+          <p className={styles.alreadyAccount}>
+            Don't have an account?<span className={styles.alreadySignin} onClick={()=>navigate('/signup')}> Sign up</span>
           </p>
         </div>
-      </form> 
+      </form>
+      </div> 
       <MessageHandler />
+    </div>
+    </div>
+    <div className={styles.rightContainer}>
+    </div>
+    </div>
     </div>
   );
 };
