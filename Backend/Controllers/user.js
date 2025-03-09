5f4b7941636735644cf4ec9a69f2e92189ac65b5
harash <harash.poriya@gmail.com>
2025-03-09 08:54:51 +0530

    add controllers for get user details by userId

--- Backend/Controllers/user.js
+++ Backend/Controllers/user.js
@@ -285,3 +285,14 @@ exports.updateProfileImageAndUserName = async(req, res) => {
         });
     }
 }
+exports.getUserDetails = async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId).select('name profileImage');
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json({ message: "Server error" });
+    }
+};
