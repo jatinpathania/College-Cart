109fd437fbb7aa31a926a4ef95b2cc23ac7b9f83
harash <harash.poriya@gmail.com>
2025-04-21 08:52:53 +0530

    route add delete method add

--- Backend/Routes/bookEchangeOrder.js
+++ Backend/Routes/bookEchangeOrder.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createBookExchangeOrder, getAllExchangeBookOrder } = require("../Controllers/bookExchangeOrder");
+const { createBookExchangeOrder, getAllExchangeBookOrder, deleteExchangeBookOrderById } = require("../Controllers/bookExchangeOrder");
 const router = express.Router();
 
 router.post("/:id/order-create-book-exchange", createBookExchangeOrder);
 router.get("/allOrderBook", getAllExchangeBookOrder)
+router.delete("/:id/deleteOrder",deleteExchangeBookOrderById)
 
 module.exports = router
